3222145aa19bc08fd648144e87c0021de274b20a
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-04-13 20:25:53 +0800

    Create Traits.ts

---
+++ src/Traits.ts
@@ -0,0 +1,9 @@
+const TRAIT_COPY = "copy";
+const TRAIT_MOVE = "move";
+
+export const Traits = {
+    "i32": TRAIT_COPY,
+    "f64": TRAIT_COPY,
+    "bool": TRAIT_COPY,
+    "String": TRAIT_MOVE
+}
