a5bd3c8eeaa1c0377063ab659c74e58adabdde56
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-04-15 23:57:50 +0800

    Add copy

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,21 @@ import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import polyfillNode from "rollup-plugin-polyfill-node";
+import copy from "rollup-plugin-copy";
 
 export default {
     // external: ['diagonjs'],
     plugins: [nodeResolve(),
     commonjs(),
     polyfillNode(),     // <---- this enables fs/path in browser
+    copy({
+      targets: [
+        {
+          src: "node_modules/diagonjs/dist/diagon.js-1.1.wasm",
+          dest: "dist"
+        }
+      ]
+    }),
     typescript()],
     input: "src/ConductorCompatibleEvaluator.ts",
     output: {
