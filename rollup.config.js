8680a87ff95984f3eebbd0cf166b9713ea1280ac
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-04-15 23:39:01 +0800

    Update ConductorCompatibleEvaluator.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,19 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-    external: ['diagonjs'],
-    plugins: [nodeResolve(), typescript()],
+    // external: ['diagonjs'],
+    plugins: [nodeResolve(),
+        commonjs(),
+    typescript()],
     input: "src/ConductorCompatibleEvaluator.ts",
     output: {
         plugins: [terser()],
         dir: "dist",
         format: "iife",
         sourcemap: true,
+        inlineDynamicImports: true, // <--- This makes it IIFE-compatible
     }
 }
