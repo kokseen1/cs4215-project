a5d3d2265646ae908628cf5476d02b98b5027cf5
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-04-21 20:02:06 +0800

    Update Traits.ts

--- src/Traits.ts
+++ src/Traits.ts
@@ -1,15 +1,14 @@
 const TRAIT_COPY = "copy";
-const TRAIT_MOVE = "move";
+const TRAIT_DROP = "move";
 
 export const Traits = {
     "i32": TRAIT_COPY,
-    "f64": TRAIT_COPY,
     "bool": TRAIT_COPY,
-    "String": TRAIT_MOVE
+    "String": TRAIT_DROP
 }
 
 export const has_move_trait = (type) =>
-    Traits[type] === TRAIT_MOVE;
+    Traits[type] === TRAIT_DROP;
 
 export const has_copy_trait = (type) =>
     Traits[type] === TRAIT_COPY;
